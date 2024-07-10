7fd3fc6782b8e9bce6d63a4c298b19fba12b495f
MenouraIlyes <ilyesmenoura19@gmail.com>
2024-07-10 15:25:01 +0200

    Explore_Screen_Done

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ya9in/datas/category_json.dart';
+import 'package:ya9in/shared/colors.dart';
+import 'package:ya9in/widgets/custom_categories_button.dart';
 import 'package:ya9in/widgets/custom_heading.dart';
+import 'package:ya9in/widgets/custom_place_holder.dart';
+import 'package:ya9in/widgets/custom_search_field.dart';
+import 'package:ya9in/widgets/custom_title.dart';
 
 class ExploreScreen extends StatefulWidget {
   const ExploreScreen({super.key});
@@ -10,12 +16,11 @@ class ExploreScreen extends StatefulWidget {
 
 class _ExploreScreenState extends State<ExploreScreen> {
   Widget getBody() {
-    var size = MediaQuery.of(context).size;
-
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(25),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Heading
             SizedBox(height: 30),
@@ -26,10 +31,52 @@ class _ExploreScreenState extends State<ExploreScreen> {
             ),
 
             // Search bar
+            SizedBox(height: 50),
+            CustomSearchField(
+              hintField: "Try 'Web Design' ",
+              backgroundColor: appWhite,
+            ),
 
             // Top Searches
+            SizedBox(height: 50),
+            CustomTitle(
+              title: 'Top Searches',
+              extend: false,
+            ),
+            SizedBox(height: 10),
+            Wrap(
+              runSpacing: 10,
+              spacing: 10,
+              children: List.generate(
+                CategoryJson.length,
+                (index) {
+                  return CustomCategoriesButton(
+                    title: CategoryJson[index]['title'],
+                  );
+                },
+              ),
+            ),
 
             // Categories
+            SizedBox(height: 50),
+            CustomTitle(
+              title: 'Categories',
+              extend: false,
+            ),
+            SizedBox(height: 10),
+            Column(
+              children: List.generate(
+                AllCategories.length,
+                (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CunstomPlaceHolder(
+                      title: AllCategories[index]['title'],
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/widgets/custom_place_holder.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ya9in/shared/colors.dart';
+
+class CunstomPlaceHolder extends StatefulWidget {
+  const CunstomPlaceHolder(
+      {Key? key, required this.title, this.isSwitch = false})
+      : super(key: key);
+  final String title;
+  final bool isSwitch;
+
+  @override
+  _CunstomPlaceHolderState createState() => _CunstomPlaceHolderState();
+}
+
+class _CunstomPlaceHolderState extends State<CunstomPlaceHolder> {
+  bool switchValue = false;
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.width * .1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            widget.title,
+            style: TextStyle(
+              fontSize: 15.0,
+              color: Colors.grey,
+            ),
+          ),
+          (widget.isSwitch)
+              ? CupertinoSwitch(
+                  value: switchValue,
+                  activeColor: appPrimary,
+                  onChanged: (bool newValue) {
+                    setState(() {
+                      switchValue = newValue;
+                    });
+                  },
+                )
+              : Icon(
+                  Icons.arrow_outward_outlined,
+                  color: appPrimary,
+                ),
+        ],
+      ),
+    );
+  }
+}
