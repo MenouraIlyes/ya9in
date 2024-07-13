e45d6b6d9ee2ed4ef56f6be5ff832b21a52428e1
MenouraIlyes <ilyesmenoura19@gmail.com>
2024-07-13 15:42:20 +0200

    Register_Screen_Done

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ya9in/shared/colors.dart';
+import 'package:ya9in/widgets/custom_button.dart';
+import 'package:ya9in/widgets/custom_heading.dart';
+import 'package:ya9in/widgets/custom_textfield.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,9 +12,123 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _phoneNumberController = TextEditingController();
+  String phoneNumberText = '';
+
   Widget getBody() {
-    return Column(
-      children: [],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // sign up header
+            SizedBox(height: 30),
+            CustomHeading(
+              title: 'Sign Up',
+              color: Colors.black,
+              subTitle: 'Welcome To yakin!',
+            ),
+
+            // sign up picture
+            SizedBox(height: 30),
+            Center(
+              child: Image.asset(
+                'assets/register.png',
+                width: 250,
+              ),
+            ),
+            Text(
+              'Please verify your phone number to get started.',
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.w500,
+                color: appPrimary,
+              ),
+            ),
+
+            // phone number textfield
+            SizedBox(height: 30),
+            CustomTextfield(
+              controller: _phoneNumberController,
+              labelText: 'Enter Phone Number',
+              phoneNumber: true,
+              onChanged: (value) {
+                setState(() {
+                  phoneNumberText = value;
+                });
+              },
+            ),
+
+            // button
+            SizedBox(height: 30),
+            CustomButtonBox(
+              title: 'Continue',
+              onTap: () {},
+              isDisabled: phoneNumberText.length < 9,
+            ),
+
+            // divider
+            SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // line
+                Expanded(
+                  child: Divider(
+                    color: Colors.grey[300],
+                  ),
+                ),
+                Text(
+                  " Or ",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey[400],
+                  ),
+                ),
+                Expanded(
+                  child: Divider(
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ],
+            ),
+
+            // google sign in box
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                width: double.infinity,
+                height: 58,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[300]!),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // google logo
+                    Image.asset(
+                      "assets/Google-Logo.png",
+                      width: 25,
+                    ),
+                    Text(
+                      'Continue with google',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -21,6 +139,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
       ),
       body: getBody(),
     );
